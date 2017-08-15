e015b1f1b444609d793fd38277ce45af702fe7df
hemusaisiva <hemusaisiva@gmail.com>
2017-08-15 15:54:02 -0700

    Anagrams Solved

--- Array Sequences/anagram_using_frquencycheck.py
+++ Array Sequences/anagram_using_frquencycheck.py
@@ -6,20 +6,24 @@ def anagram(s1,s2):
     if(len(s1) != len(s2)):
         return False
     
+    #Creating counting Dictionary
     count ={}
     
+    #Fill dictionary for first string (add counts)
     for letter in s1:
         if letter in count:
             count[letter] += 1
         else:
             count[letter] = 1
     
+    #Fill dictionary for second string (Subtract counts)
     for letter in s2:
         if letter in count:
             count[letter] -= 1
         else:
             count[letter] = 1
     
+    #check if counts are zero
     for k in count:
         if count[k] != 0:
             return False
