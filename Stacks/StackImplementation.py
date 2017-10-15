dc675e5221a082ab6df2f21132dc1c14bac0cca5
Sai Siva Hemanth Pratury <hemusaisiva@gmail.com>
2017-10-14 23:07:50 -0700

    stack Implementation using List

---
+++ Stacks/StackImplementation.py
@@ -0,0 +1,18 @@
+class Stack(object):
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def push(self, value):
+        self.items.append(value)
+
+    def pop(self):
+        return self.items.pop()
+
+    def peek(self):
+        return self.items[len(self.items) - 1]
+
+    def size(self):
+        return len(self.items)
